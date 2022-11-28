a20f4f95e9ceacc05fd117dcb07123576668ac27
Souuum <hugo.soumbrugevin@gmail.com>
2022-11-28 11:44:45 +0100

    Added new request

--- routes/users_questions_history.js
+++ routes/users_questions_history.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 
 //get every answered questions by id_user
 router.get('/user/:id', (req, res) => {
-    const id = router.params.id;
+    const id = req.params.id;
 
     db.query(
         "SELECT * FROM users_questions_history " + `WHERE id_user = ${id}`,
@@ -22,11 +22,21 @@ router.get('/user/:id', (req, res) => {
     ).then((response) => res.send(response)).catch((err) => console.log(err));
 });
 
-//get every user who responded to the questions
+//count nb of questions answered by id_user
+router.get('/user/:id/count', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        "SELECT COUNT(*) as n FROM users_questions_history " + `WHERE id_user = ${id}`,
+        { Types: QueryTypes.SELECT }
+    ).then((response) => res.send(response)).catch((err) => console.log(err));
+});
+
+//get every user who responded to the question
 
 router.get('/questions/:id', (req, res) => {
 
-    const id = router.params.id;
+    const id = req.params.id;
 
     db.query(
         "SELECT * FROM users_questions_history " + `WHERE id = ${id}`,
@@ -34,5 +44,27 @@ router.get('/questions/:id', (req, res) => {
     ).then((response) => res.send(response)).catch((err) => console.log(err));
 });
 
+//get every questions not answered by :id user ordered by poids DESC
+
+router.get('/user/:id/not-answered/', (req, res) => {
+
+    const id = req.params.id;
+
+    db.query(
+        "SELECT DISTINCT id, category, description, poids FROM questions "
+        + "WHERE id NOT IN ( SELECT id_question FROM users_questions_history "
+        + `WHERE users_questions_history.id_user = ${id}) ORDER BY poids DESC`,
+        { Types: QueryTypes.SELECT }
+    ).then((response) => res.send(response)).catch((err) => console.log(err));
+});
+
+//insert new row
+
+router.post('/addrecord', (req, res) => {
+    const params = req.body;
+    console.log(params);
+
+
+})
 
 module.exports = router;
